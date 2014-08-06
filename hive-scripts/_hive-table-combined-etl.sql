dd4e6f51c02110fe917a99cc66e0644416b5f09a
richardim <rim@rim-mbp.home>
2014-08-06 17:07:28 -0400

    column name mapping
    mapped official column names to the fake ones used.

--- hive-scripts/_hive-table-combined-etl.sql
+++ hive-scripts/_hive-table-combined-etl.sql
@@ -1,50 +1,57 @@
+#
+# Mapped to fields from source to fms types where values are relatable:
+# fms.FullName=b/d/.name
+# fms.TIN=a/b/c/d/.taxpayerid
+# fms.TotalCount=c.timeperiod
+# fms.Amount=c.loginitialgini - edited values in source
+# fms.AgencyLocationCode=c.loginitialgdppercapita - edited values in source
+# fms.RoutingNumber=c.inflation - edited values in source
+# fms.CountryCode=a.countrycode
+# Others are just labels to match fms fields
+#
+
 CREATE table finance_data_edh.combined_etl_table AS
 SELECT
-COALESCE(b.name, d.name) as name,
-COALESCE(a.taxpayerid, b.taxpayerid, c.taxpayerid, d.taxpayerid) as taxpayerid,
-a.countrycode, 
-b.country, 
-c.year, 
-c.timeperiod, 
-c.loginitialgini, 
-c.growthingini,
-c.span,
-c.loginitialgdppercapita,
-c.growthgdppercapita, 
-a.privcreavg, 
-c.logprivatecredit, 
-c.inflation, 
-c.logtrade, 
-a.gr_ltrade, 
-a.gr_school, 
-c.logschooling,
-a.logcommercialcentralbank, 
-c.loginitiallowestincomeshare,
-a.growthinlowestincomeshare, 
-d.logagedependency, 
-d.loginitialheadcount,
+COALESCE(b.name, d.name) as FullName,
+COALESCE(a.taxpayerid, b.taxpayerid, c.taxpayerid, d.taxpayerid) as TIN,
+c.year,
+c.timeperiod as TotalCount,
+c.loginitialgini as Amount,
+c.loginitialgdppercapita as AgencyLocationCode,
+c.inflation as RoutingNumber,
+a.countrycode as CountryCode,
+b.country as Country,
+c.growthingini as CheckSerialNumber,
+c.span as FederalProgramAgencyIdentifier,
+c.growthgdppercapita as SwiftReportingDetail,
+a.privcreavg as ACH_Reporting,
+c.logprivatecredit as ACH_TraceNumber,
+c.logtrade as ACH_OriginalTraceNumber,
+a.gr_ltrade as EIN,
+a.gr_school as ControlNumber,
+c.logschooling as PhoneNumber,
+a.logcommercialcentralbank as PaymentDetailTypeCode,
+c.loginitiallowestincomeshare as CheckStubLine,
+a.growthinlowestincomeshare as CheckMemoLine,
+d.logagedependency as WirePayment,
+d.loginitialheadcount as WireDetail,
+d.loginitialpovertygap as FedWireTypeCode,
+d.growthinheadcount as BankName,
+d.growthinpovertygap as BeneficiaryIdentifier,
+d.growthinmeanincome as PaymentRemark,
+d.populationgrowth as BeneficiaryBankRemarks
 
-d.loginitialpovertygap,
-d.growthinheadcount,
-d.growthinpovertygap,
-d.growthinmeanincome,
-d.populationgrowth
-
-FROM finance_data_edh.tbl_6005 A 
+FROM finance_data_edh.tbl_6005 A
 LEFT OUTER JOIN
 
-finance_data_edh.tbl_8005 B ON A.taxpayerid = B.taxpayerid 
+finance_data_edh.tbl_8005 B ON A.taxpayerid = B.taxpayerid
 RIGHT OUTER JOIN
 
-finance_data_edh.tbl_panel C ON A.taxpayerid = C.taxpayerid 
+finance_data_edh.tbl_panel C ON A.taxpayerid = C.taxpayerid
 LEFT OUTER JOIN
 
 finance_data_edh.tbl_poverty D ON A.taxpayerid = D.taxpayerid
 ;
 
-# after transform test query
-use finance_data_edh;
-select countrycode, country, 
-CASE WHEN YEAR < 1990 then YEAR ELSE 0 END AS theYEAR, 
-populationgrowth, growthinpovertygap
-FROM combined_etl_table;
+
+
